e3e943a3aea8241d850574d12dcf4e5b6333a0ae
spicyshibe <rafierasyw@gmail.com>
2025-05-13 15:46:59 +0700

    constructor.cpp done

---
+++ constructor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class mahasiswa
+{
+  public: 
+  mahasiswa(); //Constructor
+
+};
+
+mahasiswa::mahasiswa()
+{
+    cout << "Constructor Terpanggil" << endl; //Definisi Constructor
+};
+
+
+
+int main(){
+    mahasiswa mhs; //Pembuatan Object
+    return 0;
+
+}
