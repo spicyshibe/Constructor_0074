b252ce0563a6ec97136aa5cd4e7cac891ec0f207
spicyshibe <rafierasyw@gmail.com>
2025-05-13 16:19:46 +0700

    int main

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -41,3 +41,14 @@ cout << endl << "Nim =" << nim << endl;
 cout << "Nama = " << nama << endl;
 }
 
+int main() {
+    mahasiswa mhs1(102030); //hanya NIM
+    mahasiswa mhs2("andi"); //hanya NAMA
+    mahasiswa mhs3(010203, "Santi"); //NIM dan Nama 
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+    return 0;
+}
