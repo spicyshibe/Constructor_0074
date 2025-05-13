40760c3fd5094b4464b485c86a743d02db6d58b3
spicyshibe <rafierasyw@gmail.com>
2025-05-13 16:47:07 +0700

    membuat class

--- this.cpp
+++ this.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class buku{
+    string judul;
+
+    public:
+        string setget(string judul){
+            this->judul = judul; // nilai parameter 'judul' untuk member variable 'judul'
+            return this->judul; // return variable judul 
+        }
+};
+
+
