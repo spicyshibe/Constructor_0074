bf99010f5c3cacf4b3fd577213dc22e74d217d13
spicyshibe <rafierasyw@gmail.com>
2025-05-13 16:49:58 +0700

    chain function

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -4,4 +4,9 @@ using namespace std;
 class buku{
     string judul;
 
-  
+    public:
+    buku setJudul(string judul){
+        this->judul = judul;
+        return *this; //chain function
+    }
+   
